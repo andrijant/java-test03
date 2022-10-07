a496b00e4f37ece2e11282fa9c8311df07606675
andrijant <andrija_ds@yahoo.com>
2022-10-07 19:40:22 +0200

    Scanner-class

---
+++ src/javaapplication3/Test3.java
@@ -0,0 +1,21 @@
+package javaapplication3;
+
+import java.util.Scanner;
+
+public class Test3 {
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in); // System input
+        
+        System.out.print("Year of registration: "); // no new line
+              
+        
+        int yearOfRegistration = s.nextInt(); // input from Scanner s
+        
+        int yearOfGraduation = yearOfRegistration + 4;
+        System.out.println("Year of graduation: " + yearOfGraduation);
+     
+        
+    }
+    
+}
